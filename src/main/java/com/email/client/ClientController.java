65d8b5e742392d0b728a273eaff5873f6990e34a
luna <luca.barra@edu.unito.it>
2024-04-12 17:23:11 +0200

    division view

--- src/main/java/com/email/client/ClientController.java
+++ src/main/java/com/email/client/ClientController.java
@@ -82,115 +82,14 @@ public class ClientController {
 
     @FXML
     public void nuovaMail() {
-        Dialog<Pair<String, Pair<String, String>>> dialog = new Dialog<>();
-        dialog.setTitle("Nuova Email");
-        dialog.setHeaderText("Nuova email");
-
-        ButtonType confermaButton = new ButtonType("Conferma", ButtonBar.ButtonData.OK_DONE);
-        dialog.getDialogPane().getButtonTypes().addAll(confermaButton, ButtonType.CANCEL);
-
-        GridPane grid = new GridPane();
-        grid.setHgap(10);
-        grid.setVgap(10);
-        grid.setPadding(new Insets(20, 150, 10, 10));
-
-        TextField destinatarioField = new TextField();
-        destinatarioField.setPromptText("Destinatario");
-        TextField oggettoField = new TextField();
-        oggettoField.setPromptText("Oggetto");
-        TextArea testoArea = new TextArea();
-        testoArea.setPromptText("Testo");
-
-        grid.add(new Label("Destinatario:"), 0, 0);
-        grid.add(destinatarioField, 1, 0);
-        grid.add(new Label("Oggetto:"), 0, 1);
-        grid.add(oggettoField, 1, 1);
-        grid.add(new Label("Testo:"), 0, 2);
-        grid.add(testoArea, 1, 2);
-
-        dialog.getDialogPane().setContent(grid);
-
-        Platform.runLater(destinatarioField::requestFocus);
-
-        dialog.setResultConverter(dialogButton -> {
-            if (dialogButton == confermaButton) {
-                return new Pair<>(destinatarioField.getText(), new Pair<>(oggettoField.getText(), testoArea.getText()));
-            }
-            return null;
-        });
-
-
-        Optional<Pair<String, Pair<String, String>>> result = dialog.showAndWait();
-        result.ifPresent(destinatarioOggettoTesto -> {
-            String destinatario = destinatarioOggettoTesto.getKey();
-            String oggetto = destinatarioOggettoTesto.getValue().getKey();
-            String testo = destinatarioOggettoTesto.getValue().getValue();
-            if (!parseDestinatari(destinatarioField.getText())) {
-                Alert alert = new Alert(Alert.AlertType.ERROR);
-                alert.setTitle("Errore");
-                alert.setHeaderText("Email non valida");
-                alert.setContentText("Inserire un indirizzo email valido.");
-                alert.showAndWait();
-            } else {
-                Email email = new Email(LabelUsername.getText(), destinatario, oggetto, testo, LocalDateTime.now().toString());
-                System.out.println(email.getMittente() + " " + email.getDestinatario() + " " + email.getOggetto() + " " + email.getTesto() + " " + email.getData() + " ");
-                clientModel.sendEmail(email);
-            }
-        });
+        NewMailview.NuovaMail(clientModel, LabelUsername);
     }
 
     public void rispondi() {
         Email selectedEmail = emailListView.getSelectionModel().getSelectedItem();
 
         if (selectedEmail != null) {
-            Dialog<Pair<String, Pair<String, String>>> dialog = new Dialog<>();
-            dialog.setTitle("Rispondi");
-            dialog.setHeaderText("Rispondi all'email selezionata");
-
-            ButtonType confermaButton = new ButtonType("Conferma", ButtonBar.ButtonData.OK_DONE);
-            dialog.getDialogPane().getButtonTypes().addAll(confermaButton, ButtonType.CANCEL);
-
-            GridPane grid = new GridPane();
-            grid.setHgap(10);
-            grid.setVgap(10);
-            grid.setPadding(new Insets(20, 150, 10, 10));
-
-            TextField destinatarioField = new TextField(selectedEmail.getMittente());
-            destinatarioField.setDisable(true);
-            TextField oggettoField = new TextField("R: " + selectedEmail.getOggetto());
-            TextArea testoArea = new TextArea();
-            testoArea.setPromptText("Rispondi all'email qui...");
-
-            grid.add(new Label("Destinatario:"), 0, 0);
-            grid.add(destinatarioField, 1, 0);
-            grid.add(new Label("Oggetto:"), 0, 1);
-            grid.add(oggettoField, 1, 1);
-            grid.add(new Label("Testo:"), 0, 2);
-            grid.add(testoArea, 1, 2);
-
-            dialog.getDialogPane().setContent(grid);
-
-            Platform.runLater(testoArea::requestFocus);
-
-            dialog.setResultConverter(dialogButton -> {
-                if (dialogButton == confermaButton) {
-                    return new Pair<>(selectedEmail.getMittente(), new Pair<>(oggettoField.getText(), testoArea.getText()));
-                }
-                return null;
-            });
-
-            dialog.showAndWait().ifPresent(response -> {
-                if (!parseDestinatari(selectedEmail.getMittente())) {
-                    Alert alert = new Alert(Alert.AlertType.ERROR);
-                    alert.setTitle("Errore");
-                    alert.setHeaderText("Email non valida");
-                    alert.setContentText("Inserire un indirizzo email valido.");
-                    alert.showAndWait();
-                } else {
-                    Email email = new Email(LabelUsername.getText(), selectedEmail.getMittente(), response.getValue().getKey(), response.getValue().getValue(), LocalDateTime.now().toString());
-                    clientModel.sendEmail(email);
-                }
-            });
+            NewMailview.Risposta(selectedEmail, clientModel, LabelUsername);
         } else {
             Alert alert = new Alert(Alert.AlertType.WARNING);
             alert.setTitle("Nessuna email selezionata");
@@ -204,54 +103,8 @@ public class ClientController {
 
         Email selectedEmail = emailListView.getSelectionModel().getSelectedItem();
         if (selectedEmail != null) {
-            Dialog<Pair<String, Pair<String, String>>> dialog = new Dialog<>();
-            dialog.setTitle("Rispondi a tutti");
-            dialog.setHeaderText("Rispondi a tutti i destinatari dell'email selezionata");
+            NewMailview.RispostaATutti(selectedEmail, clientModel, LabelUsername);
 
-            ButtonType confermaButton = new ButtonType("Conferma", ButtonBar.ButtonData.OK_DONE);
-            dialog.getDialogPane().getButtonTypes().addAll(confermaButton, ButtonType.CANCEL);
-
-            GridPane grid = new GridPane();
-            grid.setHgap(10);
-            grid.setVgap(10);
-            grid.setPadding(new Insets(20, 150, 10, 10));
-
-            TextField destinatarioField = new TextField(selectedEmail.getMittente() + "," + selectedEmail.getDestinatario());
-            destinatarioField.setDisable(true);
-            TextField oggettoField = new TextField("R: " + selectedEmail.getOggetto());
-            TextArea testoArea = new TextArea();
-            testoArea.setPromptText("Rispondi all'email qui...");
-
-            grid.add(new Label("Destinatario:"), 0, 0);
-            grid.add(destinatarioField, 1, 0);
-            grid.add(new Label("Oggetto:"), 0, 1);
-            grid.add(oggettoField, 1, 1);
-            grid.add(new Label("Testo:"), 0, 2);
-            grid.add(testoArea, 1, 2);
-
-            dialog.getDialogPane().setContent(grid);
-
-            Platform.runLater(testoArea::requestFocus);
-
-            dialog.setResultConverter(dialogButton -> {
-                if (dialogButton == confermaButton) {
-                    return new Pair<>(selectedEmail.getMittente() + "," + selectedEmail.getDestinatario(), new Pair<>(oggettoField.getText(), testoArea.getText()));
-                }
-                return null;
-            });
-
-            dialog.showAndWait().ifPresent(response -> {
-                if (!parseDestinatari(selectedEmail.getMittente())) {
-                    Alert alert = new Alert(Alert.AlertType.ERROR);
-                    alert.setTitle("Errore");
-                    alert.setHeaderText("Email non valida");
-                    alert.setContentText("Inserire un indirizzo email valido.");
-                    alert.showAndWait();
-                } else {
-                    Email email = new Email(LabelUsername.getText(), selectedEmail.getMittente(), response.getValue().getKey(), response.getValue().getValue(), LocalDateTime.now().toString());
-                    clientModel.sendEmail(email);
-                }
-            });
         }
     }
 
---
+++ src/main/java/com/email/client/NewMailview.java
@@ -0,0 +1,187 @@
+package com.email.client;
+
+import com.email.email.Email;
+import javafx.application.Platform;
+import javafx.geometry.Insets;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+import javafx.util.Pair;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import static com.email.email.EmailParser.parseDestinatari;
+
+public class NewMailview {
+
+    public static void NuovaMail(ClientModel clientModel, Label LabelUsername){
+        Dialog<Pair<String, Pair<String, String>>> dialog = new Dialog<>();
+        dialog.setTitle("Nuova Email");
+        dialog.setHeaderText("Nuova email");
+
+        ButtonType confermaButton = new ButtonType("Conferma", ButtonBar.ButtonData.OK_DONE);
+        dialog.getDialogPane().getButtonTypes().addAll(confermaButton, ButtonType.CANCEL);
+
+        GridPane grid = new GridPane();
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(20, 150, 10, 10));
+
+        TextField destinatarioField = new TextField();
+        destinatarioField.setPromptText("Destinatario");
+        TextField oggettoField = new TextField();
+        oggettoField.setPromptText("Oggetto");
+        TextArea testoArea = new TextArea();
+        testoArea.setPromptText("Testo");
+
+        grid.add(new Label("Destinatario:"), 0, 0);
+        grid.add(destinatarioField, 1, 0);
+        grid.add(new Label("Oggetto:"), 0, 1);
+        grid.add(oggettoField, 1, 1);
+        grid.add(new Label("Testo:"), 0, 2);
+        grid.add(testoArea, 1, 2);
+
+        dialog.getDialogPane().setContent(grid);
+
+        Platform.runLater(destinatarioField::requestFocus);
+
+        dialog.setResultConverter(dialogButton -> {
+            if (dialogButton == confermaButton) {
+                return new Pair<>(destinatarioField.getText(), new Pair<>(oggettoField.getText(), testoArea.getText()));
+            }
+            return null;
+        });
+
+
+        Optional<Pair<String, Pair<String, String>>> result = dialog.showAndWait();
+        result.ifPresent(destinatarioOggettoTesto -> {
+            String destinatario = destinatarioOggettoTesto.getKey();
+            String oggetto = destinatarioOggettoTesto.getValue().getKey();
+            String testo = destinatarioOggettoTesto.getValue().getValue();
+            if (!parseDestinatari(destinatarioField.getText())) {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Errore");
+                alert.setHeaderText("Email non valida");
+                alert.setContentText("Inserire un indirizzo email valido.");
+                alert.showAndWait();
+            } else {
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Email inviata");
+                alert.setHeaderText("Email inviata con successo");
+                alert.setContentText("L'email è stata inviata con successo.");
+                alert.showAndWait();
+                Email email = new Email(LabelUsername.getText(), destinatario, oggetto, testo, LocalDateTime.now().toString());
+                System.out.println(email.getMittente() + " " + email.getDestinatario() + " " + email.getOggetto() + " " + email.getTesto() + " " + email.getData() + " ");
+                clientModel.sendEmail(email);
+            }
+        });
+    }
+
+    public static void Risposta(Email selectedEmail, ClientModel clientModel, Label LabelUsername) {
+        Dialog<Pair<String, Pair<String, String>>> dialog = new Dialog<>();
+        dialog.setTitle("Rispondi");
+        dialog.setHeaderText("Rispondi all'email selezionata");
+
+        ButtonType confermaButton = new ButtonType("Conferma", ButtonBar.ButtonData.OK_DONE);
+        dialog.getDialogPane().getButtonTypes().addAll(confermaButton, ButtonType.CANCEL);
+
+        GridPane grid = new GridPane();
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(20, 150, 10, 10));
+
+        TextField destinatarioField = new TextField(selectedEmail.getMittente());
+        destinatarioField.setDisable(true);
+        TextField oggettoField = new TextField("R: " + selectedEmail.getOggetto());
+        TextArea testoArea = new TextArea();
+        testoArea.setPromptText("Rispondi all'email qui...");
+
+        grid.add(new Label("Destinatario:"), 0, 0);
+        grid.add(destinatarioField, 1, 0);
+        grid.add(new Label("Oggetto:"), 0, 1);
+        grid.add(oggettoField, 1, 1);
+        grid.add(new Label("Testo:"), 0, 2);
+        grid.add(testoArea, 1, 2);
+
+        dialog.getDialogPane().setContent(grid);
+
+        Platform.runLater(testoArea::requestFocus);
+
+        dialog.setResultConverter(dialogButton -> {
+            if (dialogButton == confermaButton) {
+                return new Pair<>(selectedEmail.getMittente(), new Pair<>(oggettoField.getText(), testoArea.getText()));
+            }
+            return null;
+        });
+
+        dialog.showAndWait().ifPresent(response -> {
+            if (!parseDestinatari(selectedEmail.getMittente())) {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Errore");
+                alert.setHeaderText("Email non valida");
+                alert.setContentText("Inserire un indirizzo email valido.");
+                alert.showAndWait();
+            } else {
+                Email email = new Email(LabelUsername.getText(), selectedEmail.getMittente(), response.getValue().getKey(), response.getValue().getValue(), LocalDateTime.now().toString());
+                clientModel.sendEmail(email);
+            }
+        });
+    }
+
+    public static void RispostaATutti(Email selectedEmail, ClientModel clientModel, Label LabelUsername) {
+        Dialog<Pair<String, Pair<String, String>>> dialog = new Dialog<>();
+        dialog.setTitle("Rispondi a tutti");
+        dialog.setHeaderText("Rispondi a tutti i destinatari dell'email selezionata");
+
+        ButtonType confermaButton = new ButtonType("Conferma", ButtonBar.ButtonData.OK_DONE);
+        dialog.getDialogPane().getButtonTypes().addAll(confermaButton, ButtonType.CANCEL);
+
+        GridPane grid = new GridPane();
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(20, 150, 10, 10));
+
+        TextField destinatarioField = new TextField(selectedEmail.getMittente() + "," + selectedEmail.getDestinatario());
+        TextField oggettoField = new TextField("R: " + selectedEmail.getOggetto());
+        TextArea testoArea = new TextArea();
+        testoArea.setPromptText("Rispondi all'email qui...");
+
+        grid.add(new Label("Destinatario:"), 0, 0);
+        grid.add(destinatarioField, 1, 0);
+        grid.add(new Label("Oggetto:"), 0, 1);
+        grid.add(oggettoField, 1, 1);
+        grid.add(new Label("Testo:"), 0, 2);
+        grid.add(testoArea, 1, 2);
+
+        dialog.getDialogPane().setContent(grid);
+
+        Platform.runLater(testoArea::requestFocus);
+
+        dialog.setResultConverter(dialogButton -> {
+            if (dialogButton == confermaButton) {
+                return new Pair<>(selectedEmail.getMittente() + "," + selectedEmail.getDestinatario(), new Pair<>(oggettoField.getText(), testoArea.getText()));
+            }
+            return null;
+        });
+
+        dialog.showAndWait().ifPresent(response -> {
+            if (!parseDestinatari(selectedEmail.getMittente())) {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Errore");
+                alert.setHeaderText("Email non valida");
+                alert.setContentText("Inserire un indirizzo email valido.");
+                alert.showAndWait();
+            } else {
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Email inviata");
+                alert.setHeaderText("Email inviata con successo");
+                alert.setContentText("L'email è stata inviata con successo.");
+                alert.showAndWait();
+                Email email = new Email(LabelUsername.getText(), selectedEmail.getMittente(), response.getValue().getKey(), response.getValue().getValue(), LocalDateTime.now().toString());
+                clientModel.sendEmail(email);
+            }
+        });
+
+    }
+
+}
