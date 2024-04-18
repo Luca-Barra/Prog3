315ef7b68ae285f8c2c4ac2d4e53775e6ef78b23
luna <luca.barra@edu.unito.it>
2024-04-18 13:58:01 +0200

    add comments and unique ID

--- email/src/main/java/com/email/Email.java
+++ email/src/main/java/com/email/Email.java
@@ -1,7 +1,10 @@
 package com.email;
+
 import java.io.Serializable;
+import java.util.UUID;
 
 public class Email implements Serializable {
+    private final String id;
     private final String mittente;
     private final String destinatario;
     private final String oggetto;
@@ -9,7 +12,18 @@ public class Email implements Serializable {
     private final String data;
     private boolean isRead = false;
 
+    /**
+     * Costruttore della classe Email
+     * <p>
+     * @param mittente mittente dell'email
+     * @param destinatario destinatario dell'email
+     * @param oggetto oggetto dell'email
+     * @param testo testo dell'email
+     * @param data data di invio dell'email
+     */
+
     public Email(String mittente, String destinatario, String oggetto, String testo, String data) {
+        this.id = UUID.randomUUID().toString();
         this.mittente = mittente;
         this.destinatario = destinatario;
         this.oggetto = oggetto;
@@ -17,37 +31,94 @@ public class Email implements Serializable {
         this.data = data;
     }
 
+    /**
+     * Metodo per ottenere l'id dell'email
+     * <p>
+     * @return id dell'email
+     */
+
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * Metodo per ottenere il mittente dell'email
+     * <p>
+     * @return mittente dell'email
+     */
+
     public String getMittente() {
         return mittente;
     }
+
+    /**
+     * Metodo per ottenere il destinatario dell'email
+     * <p>
+     * @return destinatario dell'email
+     */
 
     public String getDestinatario() {
         return destinatario;
     }
 
+    /**
+     * Metodo per ottenere l'oggetto dell'email
+     * <p>
+     * @return oggetto dell'email
+     */
+
     public String getOggetto() {
         return oggetto;
     }
+
+    /**
+     * Metodo per ottenere il testo dell'email
+     * <p>
+     * @return testo dell'email
+     */
 
     public String getTesto() {
         return testo;
     }
 
+    /**
+     * Metodo per ottenere la data di invio dell'email
+     * <p>
+     * @return data di invio dell'email
+     */
+
     public String getData() {
         return data;
     }
+
+    /**
+     * Metodo per ottenere la rappresentazione testuale dell'email (coincide con il testo dell'email)
+     * <p>
+     * @return testo dell'email
+     */
 
     @Override
     public String toString() {
         return  testo;
     }
 
+    /**
+     * Metodo per sapere se una mail è stata letta o meno
+     * <p>
+     * @return true se la mail è stata letta, false altrimenti
+     */
+
     public boolean isRead() {
         return isRead;
     }
 
+    /**
+     * Metodo per impostare lo stato di lettura di una mail
+     * <p>
+     * @param read true se la mail è stata letta, false altrimenti
+     */
+
     public void setRead(boolean read) {
         isRead = read;
     }
-
 }
--- email/src/main/java/com/email/support/EmailParser.java
+++ email/src/main/java/com/email/support/EmailParser.java
@@ -2,9 +2,23 @@ package com.email.support;
 
 public class EmailParser {
 
+    /**
+     * Metodo per verificare se un indirizzo email è valido
+     * <p>
+     * @param email: email da verificare
+     * @return true se l'email è valida, false altrimenti
+     */
+
     public static boolean parser(String email){
         return email.matches("^[a-zA-Z0-9]+\\.[a-zA-Z0-9]+@[a-zA-Z0-9]+\\.[a-zA-Z0-9]+$");
     }
+
+    /**
+     * Metodo per verificare se una stringa di destinatari è valida
+     * <p>
+     * @param destinatari stringa di destinatari da verificare
+     * @return true se tutti i destinatari sono validi, false altrimenti
+     */
 
     public static boolean parseDestinatari(String destinatari) {
         boolean allValid = true;
@@ -18,6 +32,5 @@ public class EmailParser {
         }
         return allValid;
     }
-
 
 }
