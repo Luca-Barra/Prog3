900cfe8f87e87313b6cf3d75777979412fa7142b
luna <luca.barra@edu.unito.it>
2024-05-02 15:52:01 +0200

    separation between server model and server database

---
+++ server/src/main/java/com/email/server/database/ServerDatabase.java
@@ -0,0 +1,78 @@
+package com.email.server.database;
+
+import com.email.server.controller.ServerController;
+import com.email.server.model.ServerModel;
+import com.email.server.utils.LogEntry;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ServerDatabase {
+
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+    private static final Logger logger = Logger.getLogger(ServerModel.class.getName());
+
+    /**
+     * Metodo che salva i log in un file .txt
+     * <p>
+     * @param logEntries La lista di LogEntry nel Server
+     */
+
+    public static void saveLogs(List<LogEntry> logEntries) {
+        String filename = "server/src/main/resources/com/email/server/logs/server-logs_" + LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy")) + ".txt";
+        try {
+            System.out.println("Salvataggio dei log in corso...");
+            Path path = Paths.get(filename);
+            if (!Files.exists(path.getParent())) {
+                Files.createDirectories(path.getParent());
+            }
+            BufferedWriter writer = Files.newBufferedWriter(path, StandardOpenOption.CREATE, StandardOpenOption.APPEND);
+            writer.write("\n---------------------------------------\n");
+            writer.write("| Logs salvato il " + LocalDateTime.now().format(formatter) + " |");
+            writer.write("\n---------------------------------------\n");
+            for (LogEntry logEntry : logEntries) {
+                writer.write(logEntry.getUtente() + ";" + logEntry.getMessaggio() + ";" + logEntry.getData());
+                writer.newLine();
+            }
+            writer.close();
+        } catch (IOException e) {
+            logger.severe("Errore durante il salvataggio dei log: " + e.getMessage());
+        }
+    }
+
+    /**
+     * Metodo che carica la lista degli utenti registrati
+     * <p>
+     * @return La lista degli utenti registrati al servizio.
+     */
+
+    public static List<String> loadRegisteredUsers() {
+        List<String> registeredUsers = new ArrayList<>();
+        try(InputStream inputStream = ServerController.class.getResourceAsStream("/com/email/server/data/registeredUsers.txt")) {
+            if (inputStream != null) {
+                try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        registeredUsers.add(line);
+                    }
+                }
+            } else {
+                logger.severe("registeredUsers.txt non trovato o non accessibile");
+            }
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "Errore durante la lettura di registeredUsers.txt", e);
+        }
+        return registeredUsers;
+    }
+
+}
--- server/src/main/java/com/email/server/model/ServerModel.java
+++ server/src/main/java/com/email/server/model/ServerModel.java
@@ -1,28 +1,17 @@
 package com.email.server.model;
 
-import com.email.server.controller.ServerController;
+import com.email.server.database.ServerDatabase;
 import com.email.server.utils.LogEntry;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
-import java.io.*;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
 
 public class ServerModel {
 
     public static final ObservableList<LogEntry> logEntries = FXCollections.observableArrayList();
-    private static final Logger logger = Logger.getLogger(ServerModel.class.getName());
-    private static final ArrayList<String> registeredUsers = new ArrayList<>();
-    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+    private static ArrayList<String> registeredUsers = new ArrayList<>();
 
     /**
      * Metodo che aggiunge un log alla lista dei log
@@ -40,49 +29,13 @@ public class ServerModel {
      * Metodo che salva i log in un file .txt
      */
 
-    public static void saveLogs() {
-        String filename = "server/src/main/resources/com/email/server/logs/server-logs_" + LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy")) + ".txt";
-        try {
-            System.out.println("Salvataggio dei log in corso...");
-            Path path = Paths.get(filename);
-            if (!Files.exists(path.getParent())) {
-                Files.createDirectories(path.getParent());
-            }
-            BufferedWriter writer = Files.newBufferedWriter(path, StandardOpenOption.CREATE, StandardOpenOption.APPEND);
-            writer.write("\n---------------------------------------\n");
-            writer.write("| Logs salvato il " + LocalDateTime.now().format(formatter) + " |");
-            writer.write("\n---------------------------------------\n");
-            for (LogEntry logEntry : logEntries) {
-                writer.write(logEntry.getUtente() + ";" + logEntry.getMessaggio() + ";" + logEntry.getData());
-                writer.newLine();
-            }
-            writer.close();
-        } catch (IOException e) {
-            logger.severe("Errore durante il salvataggio dei log: " + e.getMessage());
-        }
-    }
+    public static void saveLogs() {ServerDatabase.saveLogs(logEntries);}
 
     /**
      * Metodo che carica la lista degli utenti registrati
      */
 
-    public static void loadRegisteredUsers() {
-        try(InputStream inputStream = ServerController.class.getResourceAsStream("/com/email/server/data/registeredUsers.txt")) {
-            if (inputStream != null) {
-                try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
-                    String line;
-                    while ((line = reader.readLine()) != null) {
-                        registeredUsers.add(line);
-                    }
-                }
-            } else {
-                logger.severe("registeredUsers.txt non trovato o non accessibile");
-            }
-        } catch (IOException e) {
-            logger.log(Level.SEVERE, "Errore durante la lettura di registeredUsers.txt", e);
-        }
-
-    }
+    public static void loadRegisteredUsers() {registeredUsers = (ArrayList<String>) ServerDatabase.loadRegisteredUsers();}
 
     /**
      * Metodo che controlla se un utente è registrato
