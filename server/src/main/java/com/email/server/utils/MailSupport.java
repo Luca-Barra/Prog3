e911fadf0615911ad80a474d3c3a391544f55d1e
luna <luca.barra@edu.unito.it>
2024-05-05 01:35:40 +0200

    change synchronized with lock

--- server/src/main/java/com/email/server/utils/MailSupport.java
+++ server/src/main/java/com/email/server/utils/MailSupport.java
@@ -6,23 +6,32 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 import java.util.logging.Logger;
 
 public class MailSupport {
 
     private static final Logger logger = Logger.getLogger(MailSupport.class.getName());
+    private static final ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+    private static final Lock readLock = readWriteLock.readLock();
+    private static final Lock writeLock = readWriteLock.writeLock();
 
     /**
-     * Metodo per leggere le email presenti nel file mailbox
+     * Metodo per ottenere la lista di email in arrivo
      * <p>
-     * @param mailboxFileName nome del file mailbox
-     * @return lista di email presenti nel file mailbox
-     * @throws IOException se si verifica un errore durante la lettura del file
+     * @param mailboxFileName il nome della casella di posta
+     * <p>
+     * @return La lista delle email in arrivo
+     * <p>
+     * @throws IOException Eccezioni di input/output
      */
 
-    public synchronized static List<Email> getEmails(String mailboxFileName) throws IOException {
+    public static List<Email> getEmails(String mailboxFileName) throws IOException {
         List<Email> emailList = new ArrayList<>();
 
+        readLock.lock();
         try (BufferedReader reader = new BufferedReader(new FileReader(mailboxFileName))) {
             String line;
             StringBuilder sb = new StringBuilder();
@@ -37,15 +46,18 @@ public class MailSupport {
                     sb.append("\n");
                 }
             }
+        } finally {
+            readLock.unlock();
         }
         return emailList;
     }
 
     /**
-     * Metodo per ottenere il percorso del file mailbox SENT
+     * Metodo per ottenere l'indirizzo della casella di posta con le mail inviate, ma non ricevute
      * <p>
-     * @param user nome dell'utente
-     * @return percorso del file mailbox SENT
+     * @param user L'utente di cui si vuole ottenere la casella di posta
+     * <p>
+     * @return L''indirizzo della casella di posta
      */
 
     public static String getMailboxSent(String user) {
@@ -53,17 +65,26 @@ public class MailSupport {
     }
 
     /**
-     * Metodo per ottenere il percorso del file mailbox RECEIVED
+     * Metodo per ottenere l'indirizzo della casella di posta con le mail ricevute
      * <p>
-     * @param user nome dell'utente
-     * @return percorso del file mailbox RECEIVED
+     * @param user L'utente di cui si vuole ottenere la casella di posta
+     * <p>
+     * @return L''indirizzo della casella di posta
      */
 
     public static String getMailboxReceived(String user){
         return "/home/luna/IdeaProjects/Project-Prog-3/server/src/main/resources/com/email/server/server-mailbox/RECEIVED/" + user + ".txt";
     }
 
-    public synchronized static void writeMail(String mailboxFileName, Email email) {
+    /**
+     * Metodo per scrivere su file SENT una mail
+     * <p>
+     * @param mailboxFileName Il nome della casella di posta
+     * @param email L'email
+     */
+
+    public static void writeMail(String mailboxFileName, Email email) {
+        writeLock.lock();
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(mailboxFileName, true))) {
             writer.write(email.getMittente() + ";"
                     + email.getDestinatario() + ";"
@@ -73,17 +94,20 @@ public class MailSupport {
                     "\n");
         } catch (IOException e) {
             logger.severe("Errore durante la scrittura dell'email: " + e.getMessage());
+        } finally {
+            writeLock.unlock();
         }
     }
 
     /**
-     * Metodo per salvare le nuove email ricevute nel file mailbox RECEIVED
+     * Metodo per scrivere su file RECEIVED una mail
      * <p>
-     * @param mailboxFileNameReceived nome del file mailbox RECEIVED
-     * @param emailList lista di email da salvare
+     * @param mailboxFileNameReceived Il nome della casella di posta
+     * @param emailList La lista di email
      */
 
-    public synchronized static void saveNewEmails(String mailboxFileNameReceived, List<Email> emailList) {
+    public static void saveNewEmails(String mailboxFileNameReceived, List<Email> emailList) {
+        writeLock.lock();
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(mailboxFileNameReceived, true))) {
             for (Email email : emailList) {
                 writer.write(email.getMittente() + ";"
@@ -95,18 +119,21 @@ public class MailSupport {
             }
         } catch (IOException e) {
             logger.severe("Errore durante il salvataggio delle email: " + e.getMessage());
+        } finally {
+            writeLock.unlock();
         }
     }
 
     /**
-     * Metodo per eliminare un'email dal file mailbox
+     * Metodo per cancellare definitivamente una mail da RECEIVED
      * <p>
-     * @param mailboxFileName nome del file mailbox
-     * @param email email da eliminare
+     * @param mailboxFileName Il nome della casella di posta
+     * @param email L'email da cancellare
      */
 
-    public synchronized static void deleteEmail(String mailboxFileName, Email email) {
+    public static void deleteEmail(String mailboxFileName, Email email) {
         List<Email> emailList = new ArrayList<>();
+        readLock.lock();
         try (BufferedReader reader = new BufferedReader(new FileReader(mailboxFileName))) {
             String line;
             while ((line = reader.readLine()) != null) {
@@ -118,22 +145,26 @@ public class MailSupport {
             }
         } catch (IOException e) {
             logger.severe("Errore durante la lettura dell'email: " + e.getMessage());
+        } finally {
+            readLock.unlock();
         }
 
-        emailList.removeIf(e -> Objects.equals(e.getId(), email.getId()));
-
+        writeLock.lock();
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(mailboxFileName, false))) {
             for (Email e : emailList) {
-                writer.write(e.getMittente() + ";"
-                        + e.getDestinatario() + ";"
-                        + e.getOggetto() + ";"
-                        + e.getTesto() + ";"
-                        + e.getData() + ";"
-                        + e.getId() + "\n");
+                if (!Objects.equals(e.getId(), email.getId())) {
+                    writer.write(e.getMittente() + ";"
+                            + e.getDestinatario() + ";"
+                            + e.getOggetto() + ";"
+                            + e.getTesto() + ";"
+                            + e.getData() + ";"
+                            + e.getId() + "\n");
+                }
             }
         } catch (IOException e) {
             logger.severe("Errore durante la scrittura dell'email: " + e.getMessage());
+        } finally {
+            writeLock.unlock();
         }
     }
-
 }
